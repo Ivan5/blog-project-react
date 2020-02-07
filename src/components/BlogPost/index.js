55f2b21014e4603549037008f10dd0fa4eef0937
Ivan5 <converse_ivan@hotmail.com>
2020-02-07 13:24:47 -0600

    Make a sidebar and blogpost style

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -3,5 +3,20 @@ import "./style.css";
 import Card from "../UI/Card";
 
 export default function BlogPost() {
-  return <Card style={{ width: "70%" }}>My first BlogPost</Card>;
+  return (
+    <div className="blogPostContainer">
+      <Card>
+        <div className="blogHeader">
+          <span className="blogCategory">Featured</span>
+          <h1 className="postTitle">Beautiful is always beuatiful</h1>
+          <span className="postedBy">
+            posted on July 21,2016 by Sora Blogging Tips
+          </span>
+        </div>
+        <div className="postImageContainer">
+          <img src={require("../../blogPostImages/memories-from.jpg")} alt="" />
+        </div>
+      </Card>
+    </div>
+  );
 }
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,5 +3,34 @@ import "./style.css";
 import Card from "../UI/Card";
 
 export default function SideBar() {
-  return <Card style={{ width: "27%" }}>SideBar</Card>;
+  return (
+    <div className="sidebarContainer">
+      <Card
+        style={{
+          marginBottom: "20px",
+          padding: "20px",
+          boxSizing: "border-box"
+        }}
+      >
+        <div className="cardHeader">
+          <span>About Us</span>
+        </div>
+        <div className="profileImageContainer">
+          <img
+            src="https://pbs.twimg.com/profile_images/1084565493774598145/rlA4r7BE_400x400.jpg"
+            alt=""
+          />
+        </div>
+        <div className="cardBody">
+          <p>My name is Ivan JF I am a software developer</p>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="cardHeader">
+          <span>Social Network</span>
+        </div>
+      </Card>
+    </div>
+  );
 }
