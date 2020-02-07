15d18eae3118196f041b7d813854f6ba4d7d2a94
Ivan5 <converse_ivan@hotmail.com>
2020-02-07 11:33:30 -0600

    Make some changes in Navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function Navbar() {
+  const [search, setSearch] = useState(false);
+
+  const submitSearch = e => {
+    e.preventDefault();
+  };
+
+  const openSearch = () => {
+    setSearch(true);
+  };
+
+  const searchClass = search ? "searchInput active" : "searchInput";
+
   return (
     <div className="navbar">
       <ul className="navbarMenu">
@@ -19,7 +31,9 @@ function Navbar() {
         </li>
       </ul>
       <div className="search">
-        <input type="text" placeholder="Search" />
+        <form onSubmit={submitSearch}>
+          <input type="text" placeholder="Search" className={searchClass} />
+        </form>
       </div>
     </div>
   );
