7e18a170fe1a555e9092566bf10bdc69c6993612
Ivan5 <converse_ivan@hotmail.com>
2020-02-11 16:27:06 -0600

    Refactor some code in Home component view

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,6 +4,42 @@ import "./style.css";
 import Card from "../../components/UI/Card";
 import SideBar from "../../components/SideBar";
 import RecentPost from "./RecentPost";
+import blogData from "../../data/blog.json";
+
+const SideImage = props => {
+  return (
+    <div style={{ height: `${props.height}px` }}>
+      <img src={props.src} alt="" />
+    </div>
+  );
+};
+
+const ImageGallary = props => (
+  <div className="galleryPost" style={props.galleryStyle}>
+    <section style={{ width: "70%" }}>
+      <div className="mainImageWrapper">
+        <img
+          src={require("../../blogPostImages/" + props.imagesArray[0])}
+          alt=""
+        />
+      </div>
+    </section>
+    <section className="sideImageWrapper" style={{ width: "30%" }}>
+      <SideImage
+        height={props.sideImageHeight}
+        src={require("../../blogPostImages/" + props.imagesArray[1])}
+      />
+      <SideImage
+        height={props.sideImageHeight}
+        src={require("../../blogPostImages/" + props.imagesArray[2])}
+      />
+      <SideImage
+        height={props.sideImageHeight}
+        src={require("../../blogPostImages/" + props.imagesArray[3])}
+      />
+    </section>
+  </div>
+);
 
 const Home = props => {
   const galleryHeight = 450;
@@ -14,42 +50,21 @@ const Home = props => {
 
   const sideImageHeight = galleryHeight / 3;
 
+  const imgAr = blogData.data.map(post => post.blogImage);
+
   return (
     <div>
       <Card style={{ margin: "20px 0" }}>
-        <div className="galleryPost" style={{ galleryStyle }}>
-          <section style={{ width: "70%" }}>
-            <div className="">
-              <img
-                src={require(`../../blogPostImages/memories-from.jpg`)}
-                alt=""
-              />
-            </div>
-          </section>
-          <section className="sideImageWrapper" style={{ width: "30%" }}>
-            <div style={{ height: `${sideImageHeight}px` }}>
-              <img
-                src={require(`../../blogPostImages/memories-from.jpg`)}
-                alt=""
-              />
-            </div>
-            <div style={{ height: `${sideImageHeight}px` }}>
-              <img
-                src={require(`../../blogPostImages/memories-from.jpg`)}
-                alt=""
-              />
-            </div>
-            <div style={{ height: `${sideImageHeight}px` }}>
-              <img
-                src={require(`../../blogPostImages/memories-from.jpg`)}
-                alt=""
-              />
-            </div>
-          </section>
-        </div>
+        <ImageGallary
+          largeWidth="70%"
+          smallWidth="30%"
+          sideImageHeight={sideImageHeight}
+          galleryStyle={galleryStyle}
+          imagesArray={imgAr}
+        />
       </Card>
       <section className="HomeContainer">
-        <RecentPost></RecentPost>
+        <RecentPost style={{ width: "70%" }}></RecentPost>
         <SideBar></SideBar>
       </section>
     </div>
